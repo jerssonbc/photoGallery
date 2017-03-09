b2dbaefea3e0f005f6031447cdea5a5b07dea659
Jersson Bacilio <jerss.edu@gmail.com>
2017-03-08 22:58:37 -0500

    Creando index y parser

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Multi File UpLoad</title>
+	<link rel="stylesheet" href="css/dropzone.css" type="text/css" rel="stylesheet" />
+	<script src="js/dropzone.js"></script>
+</head>
+<body>
+	<form action="parser.php" class="dropzone"></form>
+	<p><a href="view_upload.php">Vista de subida</a></p>
+</body>
+</html>
---
+++ view_upload.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Vista de subida</title>
+</head>
+<body>
+	<p><a href="index.php">Atras</a></p>
+</body>
+</html>
