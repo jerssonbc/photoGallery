1b4e24c52d85cb317e456e7e7d5c069730f19066
Jersson Bacilio <jerss.edu@gmail.com>
2017-03-08 22:59:06 -0500

    Creando view_upload

--- view_upload.php
+++ view_upload.php
@@ -5,6 +5,21 @@
 	<title>Vista de subida</title>
 </head>
 <body>
+	<?php 
+		$folder = "uploads";
+
+		if(is_dir($folder)){
+			$handler = opendir($folder);
+			$output = "";
+
+			while($files = readdir($handler)){
+				if(!is_dir($files)){
+					$output .= "<img src=\"uploads/{$files}\" width='180' height='180'>";
+				}
+			}
+		}
+		echo $output;
+	 ?>
 	<p><a href="index.php">Atras</a></p>
 </body>
 </html>
